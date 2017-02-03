62ad83710d59ef5f36cda83b2cafd432d3c63b24
chelseadlee <chelsea.d.lee12@gmail.com>
2017-02-03 12:50:40 -0800

    add knockout framework with todos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,45 @@
-// var ViewModel = function() {
+var PlaceList = function(data) {
+    this.name = ko.observable(data.name);
+    this.address = ko.observable(data.address);
+};
 
-// }
-// ko.applyBindings(new ViewModel());
+var Place = function(place){
+    //create observable properties from return location
+    //this is Cat from catclicker
+}
+
+//locations model data should be passed
+var PlaceListViewModel = function(googleMapsMarkers) {
+    var self = this;
+
+    //delete this line markers don't need to be observed since not data
+    self.markers = ko.observableArray(googleMapsMarkers);
+    //create observable array of Place instances
+    //
+
+
+    self.filter = ko.observable("");
+    // filter the marker locations using ko utility filter
+    filteredPlaces = ko.computed(function() {
+        var filter = self.filter().toLowerCase();
+        if (!filter) {
+            return self.markers();
+        } else {
+            return ko.utils.arrayFilter(self.items(), function(marker) {
+                return ko.utils.indexOf(place.name().toLowerCase(), filter);
+            });
+        }
+    }, PlaceListViewModel);
+
+    self.places = ko.observableArray([]);
+    self.newPlace = ko.observable();
+
+    markers.forEach(function(placeItem) {
+        self.places.push (new Place(placeItem));
+    });
+}
+
+
 
 // Google Maps API
 var map;
@@ -28,12 +66,18 @@ function initMap() {
 
     service = new google.maps.places.PlacesService(map);
     service.nearbySearch(request, findLocations);
+    //once locations array initialized in findLocations callback
+    //the view model can be initialized pass into the constructor function
+    ko.applyBindings(new PlaceListViewModel(markers));//markers should be locations
+
 }
 
 function findLocations(results, status){
     if (status == google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < results.length; i++) {
             var place = results[i];
+            //store returned places in locations array, this is your data/model
+
             createMarker(results[i]);
             markers.push(results[i]);
         }
@@ -54,21 +98,20 @@ function createMarker(place) {
                 return;
             }
             // populateInfoWindow(this, infowindow);
-            console.log("it worked " + result.name);
             infowindow.setContent(result.name);
+            console.log(JSON.stringify(result));
             infowindow.open(map, marker);
         });
     });
 }
-
-function populateInfoWindow(marker, infowindow) {
-    // if (infowindow.marker == marker) {
-    //     infowindow.setContent('');
-    //         infowindow.marker = marker;
-    //         infowindow.addListener('closeclick', function() {
-    //             infowindow.marker = null;
-    //         });
-    //         infowindow.open(map, marker);
-    // }
-    infowindow.open(map, marker);
-}
+// function populateInfoWindow(marker, infowindow) {
+//     // if (infowindow.marker == marker) {
+//     //     infowindow.setContent('');
+//     //         infowindow.marker = marker;
+//     //         infowindow.addListener('closeclick', function() {
+//     //             infowindow.marker = null;
+//     //         });
+//     //         infowindow.open(map, marker);
+//     // }
+//     infowindow.open(map, marker);
+// }
